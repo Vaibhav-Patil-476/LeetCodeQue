4e1b3e1940d0c1463afe67cf6dc3eb361bcd5ca2
Vaibhav Patil <vai044131@gmail.com>
2025-12-10 13:10:21 +0530

    Pattern_Pyramid problem solved

---
+++ src/Com/Leetcode/Pattern_Pyramid.java
@@ -0,0 +1,32 @@
+package Com.Leetcode;
+
+import java.util.Scanner;
+
+public class Pattern_Pyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter a number:");
+		
+		int num=sc.nextInt();
+		
+		//for outer loop 
+				for(int i=1;i<=num;i++) {
+					
+					for(int j=1;j<=num-i;j++) {
+						System.out.print(" ");
+					}
+						
+						for(int k=1;k<=(2*i-1);k++) {
+							System.out.print("*");
+					}
+						
+//				System.out.println();	
+					
+				}
+				
+	}
+
+}
