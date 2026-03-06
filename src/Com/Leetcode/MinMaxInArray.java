522ec86559e2d47e3bb82ccb1c44dd93479b960c
Patil Vaibhav <vai044131@gmail.com>
2026-03-06 22:56:27 +0530

    Make some changes

--- src/Com/Leetcode/MinMaxInArray.java
+++ src/Com/Leetcode/MinMaxInArray.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 		int minElement = arr[0];
 		int maxElement = arr[0];
 		
+		//for loop 
 		for(int i = 1; i < arr.length; i++)
 		{
 			if(maxElement < arr[i])
